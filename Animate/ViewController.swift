a17646b40215e0c29017d06d845b41064ba0afb3
Henry <henc313@gmail.com>
2019-10-15 11:49:07 +0300

    Initial Commit

---
+++ Animate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animate
+//
+//  Created by ♏︎ on 10/15/19.
+//  Copyright © 2019 Henry Kivimaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
