f0ef3e5474423fcde6807d581375d9ecb8024f77
Henry <henc313@gmail.com>
2019-10-17 11:03:36 +0300

    Move panGesture Method from ViewController to CardView

--- Animate/CardView.swift
+++ Animate/CardView.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class CardView: UIView {   
+class CardView: UIView {
    
+   let mainView = UIScreen.main.bounds
+   
+   //MARK:- Initializers
    override init(frame: CGRect) {
       super.init(frame: frame)
       addSubview(titleLabel)
@@ -17,17 +20,25 @@ class CardView: UIView {
       addSubview(imageView)
       addSubview(thumbImageView)
       configureCard()
-      setupLabelViewConstraints()
-      setupSubtitleLabelViewConstraints()
-      setupImageViewViewConstraints()
-      setupThumbImageViewViewConstraints()
+      setupCardViewConstraints()
+      setupLabelConstraints()
+      setupSubtitleLabelConstraints()
+      setupImageViewConstraints()
+      setupThumbImageViewConstraints()
    }
    
    required init?(coder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
    }
    
-   fileprivate func setupLabelViewConstraints() {
+   //MARK:- Constraints
+   fileprivate func setupCardViewConstraints() {
+      self.translatesAutoresizingMaskIntoConstraints                      = false
+      self.heightAnchor.constraint(equalToConstant: 460).isActive         = true
+      self.widthAnchor.constraint(equalToConstant: 330).isActive          = true
+   }
+   
+   fileprivate func setupLabelConstraints() {
       titleLabel.translatesAutoresizingMaskIntoConstraints                                   = false
       titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 0).isActive = true
       titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive        = true
@@ -35,7 +46,7 @@ class CardView: UIView {
       titleLabel.heightAnchor.constraint(equalToConstant: 64).isActive                       = true
    }
    
-   fileprivate func setupSubtitleLabelViewConstraints() {
+   fileprivate func setupSubtitleLabelConstraints() {
       subtitleLabel.translatesAutoresizingMaskIntoConstraints                                     = false
       subtitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: 0).isActive   = true
       subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
@@ -43,7 +54,7 @@ class CardView: UIView {
       subtitleLabel.heightAnchor.constraint(equalToConstant: 20).isActive                         = true
    }
    
-   fileprivate func setupImageViewViewConstraints() {
+   fileprivate func setupImageViewConstraints() {
       imageView.translatesAutoresizingMaskIntoConstraints                                        = false
       imageView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 20).isActive = true
       imageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30).isActive           = true
@@ -53,7 +64,7 @@ class CardView: UIView {
       
    }
    
-   fileprivate func setupThumbImageViewViewConstraints() {
+   fileprivate func setupThumbImageViewConstraints() {
       thumbImageView.translatesAutoresizingMaskIntoConstraints                                        = false
       thumbImageView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive      = true
       thumbImageView.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
@@ -61,6 +72,7 @@ class CardView: UIView {
       thumbImageView.heightAnchor.constraint(equalToConstant: 140).isActive = true
    }
    
+   //MARK:- Views
    let titleLabel: UILabel = {
       let label               = UILabel(frame: CGRect(x: 10, y: 10, width: 80, height: 44))
       label.layer.borderWidth = 1.0
@@ -95,13 +107,54 @@ class CardView: UIView {
       return thumbImageView
    }()
    
-   
+   //MARK:- Methods
    func configureCard() {
       layer.cornerRadius  = 12
       layer.masksToBounds = true
       backgroundColor     = UIColor(white: 0, alpha: 0.8)
+      self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panCard)))
    }
    
+   @objc func panCard(_ sender: UIPanGestureRecognizer) {
+      guard let card = sender.view else { return }
+      let point = sender.translation(in: card)
+      let xFromCenter = card.center.x - UIScreen.main.bounds.midX
+      
+      card.center = CGPoint(x: UIScreen.main.bounds.midX + point.x, y: UIScreen.main.bounds.midY)
+      
+      card.transform = CGAffineTransform(rotationAngle: (xFromCenter * (0.4 / UIScreen.main.bounds.width))).translatedBy(x: 0, y: abs(xFromCenter) * (50 / UIScreen.main.bounds.width))
+      if xFromCenter > 0 {
+         self.thumbImageView.image = #imageLiteral(resourceName: "thumbUp")
+      } else {
+         self.thumbImageView.image = #imageLiteral(resourceName: "thumbDown")
+      }
+      
+      self.thumbImageView.alpha = abs(xFromCenter / UIScreen.main.bounds.midX)
+      
+      if sender.state == .ended {
+         if card.center.x < 0 {
+            UIView.animate(withDuration: 0.3, animations: {
+               card.center = CGPoint(x: card.center.x - (card.frame.width / 2), y: card.center.y)
+               card.alpha = 0
+            }) { (true) in
+               self.removeFromSuperview()
+            }
+         } else if card.center.x > self.frame.width {
+            UIView.animate(withDuration: 0.3, animations:  {
+               card.center = CGPoint(x: card.center.x + (card.frame.width / 2), y: card.center.y)
+               card.alpha = 0
+            }) { (true) in
+               self.removeFromSuperview()
+            }
+         } else {
+            UIView.animate(withDuration: 0.2) {
+               card.center = CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY)
+               card.transform = .identity
+               self.thumbImageView.alpha = 0
+            }
+         }
+      }
+   }
    
    
 }
--- Animate/SecondViewController.swift
+++ Animate/SecondViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
    
+   //MARK:- Outlets
    @IBOutlet weak var backgroundImageView: UIImageView!
    @IBOutlet weak var gradientView: UIView!
    
@@ -22,74 +23,25 @@ class SecondViewController: UIViewController {
    @IBOutlet weak var twitterButton: UIButton!
    @IBOutlet weak var facebookButton: UIButton!
    
+   var cardView: CardView?
+   var visibleCards = 3
+   var cardsStack: [CardView]?
    
+   //MARK:- View Load Methods
    override func viewDidLoad() {
       super.viewDidLoad()
       gradientView.setGradient(firstColor: UIColor(white: 0, alpha: 0.0), secondColor: UIColor(white: 0, alpha: 0.3))
       setUpButtonDrawer()
-      cardView.subtitleLabel.text = "Photo by Rob Robinson"
-      view.addSubview(cardView)
-      setupCardViewConstraints()
-      cardView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panCard)))
+      
+      cardView = CardView()
+      cardView?.subtitleLabel.text = "Photo by Rob Robinson"
+      view.addSubview(cardView!)
    }
    
-   let cardView: CardView = {
-      let view = CardView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-      view.layer.borderWidth = 4.0
-      view.layer.borderColor = UIColor.lightGray.cgColor
-      return view
-   }()
-   
-   fileprivate func setupCardViewConstraints() {
-      cardView.translatesAutoresizingMaskIntoConstraints                      = false
-      cardView.heightAnchor.constraint(equalToConstant: 460).isActive         = true
-      cardView.widthAnchor.constraint(equalToConstant: 330).isActive          = true
-      cardView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-      cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+   func configureCardPositionInView() {
+      cardView?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+      cardView?.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
    }
-   
-   
-   @objc func panCard(_ sender: UIPanGestureRecognizer) {
-      guard let card = sender.view else { return }
-      let point = sender.translation(in: card)
-      let xFromCenter = card.center.x - view.center.x
-
-      card.center = CGPoint(x: view.center.x + point.x, y: view.center.y)
-
-      card.transform = CGAffineTransform(rotationAngle: (xFromCenter * (0.4 / view.frame.width))).translatedBy(x: 0, y: abs(xFromCenter) * (50 / view.frame.width))
-      if xFromCenter > 0 {
-         cardView.thumbImageView.image = #imageLiteral(resourceName: "thumbUp")
-      } else {
-         cardView.thumbImageView.image = #imageLiteral(resourceName: "thumbDown")
-      }
-
-      cardView.thumbImageView.alpha = abs(xFromCenter / view.center.x)
-
-      if sender.state == .ended {
-         if card.center.x < 0 {
-            UIView.animate(withDuration: 0.3, animations: {
-               card.center = CGPoint(x: card.center.x - (card.frame.width / 2), y: card.center.y)
-               card.alpha = 0
-            }) { (true) in
-               self.cardView.removeFromSuperview()
-            }
-         } else if card.center.x > view.frame.width {
-            UIView.animate(withDuration: 0.3, animations:  {
-               card.center = CGPoint(x: card.center.x + (card.frame.width / 2), y: card.center.y)
-               card.alpha = 0
-            }) { (true) in
-               self.cardView.removeFromSuperview()
-            }
-         } else {
-            UIView.animate(withDuration: 0.2) {
-               card.center = self.view.center
-               card.transform = .identity
-               self.cardView.thumbImageView.alpha = 0
-            }
-         }
-      }
-   }
-   
    
    @IBAction func toggleMenuTapped(_ sender: UIButton) {
       if buttonBackgroundView.transform.isIdentity {
@@ -134,6 +86,7 @@ class SecondViewController: UIViewController {
       buttonBackgroundView.layer.cornerRadius = 22
       buttonBackgroundView.backgroundColor    = UIColor(white: 0.0, alpha: 0.5)
       buttonContainerView.clipsToBounds       = true
+      
       instagramButton.alpha = 0
       emailButton.alpha     = 0
       twitterButton.alpha   = 0
